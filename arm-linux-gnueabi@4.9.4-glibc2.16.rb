d7cf322ecdbdea30792b316ae2d8b37140c86aa8
Meowlgmee <87848034+meowlgmee@users.noreply.github.com>
2024-10-21 13:53:22 +0800

    Update arm-linux-gnueabi@4.9.4-glibc2.16.rb
    Revise archive download url

--- arm-linux-gnueabi@4.9.4-glibc2.16.rb
+++ arm-linux-gnueabi@4.9.4-glibc2.16.rb
@@ -10,7 +10,7 @@ class CpuRequirement < Requirement
 
 class ArmLinuxGnueabiAT494Glibc216 < Formula
     desc "aarch64 Linux GNU Toolchain"
-    homepage "https://github.com/nexbeam/homebrew-macos-cross-toolchains"
+    homepage "https://github.com/meowlgmee/homebrew-macos-cross-toolchains"
     license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
     version "4.9.4"
     revision 1
@@ -19,7 +19,7 @@ class ArmLinuxGnueabiAT494Glibc216 < Formula
         depends_on CpuRequirement
     end
   
-    url "https://github.com/nexbeam/homebrew-macos-cross-toolchains/releases/download/v4.9.4-glibc2.16/arm-linux-gnueabi-aarch64-darwin.tar.gz"
+    url "https://github.com/meowlgmee/homebrew-macos-cross-toolchains/releases/download/v4.9.4-glibc2.16/arm-linux-gnueabi-aarch64-darwin.tar.gz"
     sha256 "86f7fac6613a1a2abe3c60694ceccacad5745ba042bafd25d834407fbda95e65"
   
     def install
