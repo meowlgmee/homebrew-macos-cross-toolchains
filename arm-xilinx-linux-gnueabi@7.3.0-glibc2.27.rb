798c0fed1fdf791391a90cbc12f974ead77b2d13
Dingtao <dingtao.cpp@foxmail.com>
2025-05-30 16:28:03 +0800

    bugfix: fix sha256 for toolchain v7.3.0 armv7

--- arm-xilinx-linux-gnueabi@7.3.0-glibc2.27.rb
+++ arm-xilinx-linux-gnueabi@7.3.0-glibc2.27.rb
@@ -20,7 +20,7 @@ class ArmXilinxLinuxGnueabiAT730Glibc227 < Formula
     end
   
     url "https://github.com/meowlgmee/homebrew-macos-cross-toolchains/releases/download/v7.3.0-glibc2.27-armv7/arm-xilinx-linux-gnueabi-v7.3.0-host-aarch64-darwin.tar.gz"
-    sha256 "f9c22f053e8e353a1e6d823f674015beb54063087b79f0e2c0f5e85e91331574"
+    sha256 "ac31f7d612158ba566b235d54e3ddbbb7390ec93ad37246e4fd64503a4a25216"
   
     def install
     (prefix/"toolchain").install Dir["./*"]
