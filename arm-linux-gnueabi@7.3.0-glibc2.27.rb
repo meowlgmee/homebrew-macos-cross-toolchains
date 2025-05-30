dcf192782c9ffff1ea6ce93c931ceeb3491dfef5
Dingtao <dingtao.cpp@foxmail.com>
2025-05-30 15:15:12 +0800

    add armv7-gcc-7.3.0 with glibc 2.27

---
+++ arm-linux-gnueabi@7.3.0-glibc2.27.rb
@@ -0,0 +1,29 @@
+class CpuRequirement < Requirement
+    fatal true
+  
+    satisfy() { Hardware::CPU.arm? }
+  
+    def message
+      "Only apple m1/m2 supported!"
+    end
+  end
+
+class ArmLinuxGnueabiAT730Glibc217 < Formula
+    desc "aarch64 Linux GNU Toolchain"
+    homepage "https://github.com/meowlgmee/homebrew-macos-cross-toolchains"
+    license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
+    version "7.3.0"
+    revision 1
+
+    on_macos do
+        depends_on CpuRequirement
+    end
+  
+    url "https://github.com/meowlgmee/homebrew-macos-cross-toolchains/releases/download/v7.3.0-glibc2.27-armv7/arm-linux-gnueabi-v7.3.0-host-aarch64-darwin.tar.gz"
+    sha256 "f9c22f053e8e353a1e6d823f674015beb54063087b79f0e2c0f5e85e91331574"
+  
+    def install
+    (prefix/"toolchain").install Dir["./*"]
+    Dir.glob(prefix/"toolchain/bin/*") {|file| bin.install_symlink file}
+    end
+  end
